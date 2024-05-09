8712806d98d3deb50162ff0e54179750c01e9687
Felipesdr <felipederose@gmail.com>
2024-05-09 16:58:22 -0300

    Add  getAllCategoryByIdUserAndGlobal method to CategoryService.

--- src/main/java/com/lhama/lhamapersonalfinances/services/CategoryService.java
+++ src/main/java/com/lhama/lhamapersonalfinances/services/CategoryService.java
@@ -2,22 +2,24 @@ package com.lhama.lhamapersonalfinances.services;
 
 import com.lhama.lhamapersonalfinances.entities.category.*;
 import com.lhama.lhamapersonalfinances.entities.user.User;
-import com.lhama.lhamapersonalfinances.repositorys.CategoryRespository;
+import com.lhama.lhamapersonalfinances.repositorys.CategoryRepository;
 import com.lhama.lhamapersonalfinances.repositorys.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
 
     @Autowired
-    CategoryRespository categoryRespository;
+    CategoryRepository categoryRepository;
     @Autowired
     UserRepository userRepository;
 
     public Category registerCategory(CategoryRegisterDTO categoryRegisterData){
         Category newCategory = new Category(categoryRegisterData);
-        newCategory = categoryRespository.save(newCategory);
+        newCategory = categoryRepository.save(newCategory);
 
         return newCategory;
     }
@@ -25,13 +27,19 @@ public class CategoryService {
     public Category registerCategoryCreatedByUser(CategoryRegisterCreatedByUserDTO categoryRegisterCreatedByUserData){
         User user =  userRepository.findById(categoryRegisterCreatedByUserData.idUser()).orElseThrow();
         Category newCategory = new Category(categoryRegisterCreatedByUserData, user);
-        newCategory = categoryRespository.save(newCategory);
+        newCategory = categoryRepository.save(newCategory);
 
         return newCategory;
     }
 
+    public List<Category> getAllCategoryByIdUserAndGlobal(Integer idUser){
+        User user = userRepository.findById(idUser).orElseThrow();
+
+       return categoryRepository.findAllByUserOrUserIsNullAndActiveTrue(user);
+    }
+
     public Category updateCategoryById(CategoryUpdateDTO categoryUpdateData){
-        Category updatedCategory = categoryRespository.getReferenceById(categoryUpdateData.idCategory());
+        Category updatedCategory = categoryRepository.getReferenceById(categoryUpdateData.idCategory());
         updatedCategory.updateCategory(categoryUpdateData);
         return updatedCategory;
 
