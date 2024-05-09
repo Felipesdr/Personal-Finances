887ebb98db328786c3cd729b4dafed6adba45a6e
Felipesdr <felipederose@gmail.com>
2024-05-09 16:57:00 -0300

    Add  findAllByUserOrUserIsNullAndActiveTrue method to CategoryRepository.

--- src/main/java/com/lhama/lhamapersonalfinances/repositorys/CategoryRepository.java
+++ src/main/java/com/lhama/lhamapersonalfinances/repositorys/CategoryRepository.java
@@ -1,13 +1,15 @@
 package com.lhama.lhamapersonalfinances.repositorys;
 
 import com.lhama.lhamapersonalfinances.entities.category.Category;
+import com.lhama.lhamapersonalfinances.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CategoryRespository extends JpaRepository<Category, Integer> {
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
-    public List<Category> findAllByActiveTrue();
+    public List<Category> findAllByUserOrUserIsNullAndActiveTrue(User user);
+
 }
