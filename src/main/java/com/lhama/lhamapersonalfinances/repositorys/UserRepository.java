418e366bcd59e735aff356c9e682156b8f131150
Felipesdr <felipederose@gmail.com>
2024-05-10 11:18:24 -0300

    Add UserRole

--- src/main/java/com/lhama/lhamapersonalfinances/repositorys/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lhama.lhamapersonalfinances.repositorys;
-
-import com.lhama.lhamapersonalfinances.entities.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
