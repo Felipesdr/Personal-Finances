0471065e4b84dfe71cdd44aa236624849aeea2ea
Felipesdr <felipederose@gmail.com>
2024-05-11 19:24:16 -0300

    Add method existsByNameAndActiveTrue to CategoryRepository

--- src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/CategoryRepository.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/CategoryRepository.java
@@ -12,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
     public List<Category> findAllByUserAndActiveTrueOrUserIsNullAndActiveTrue(User user);
 
+    public boolean existsByNameAndActiveTrue(String name);
+
 }
