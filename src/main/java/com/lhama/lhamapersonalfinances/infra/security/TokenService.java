622755c92deee47909bfe5ca30222b7ecf99c9bd
Felipesdr <felipederose@gmail.com>
2024-05-12 09:38:00 -0300

    Add recoverIdFromToken to TokenService.

--- src/main/java/com/lhama/lhamapersonalfinances/infra/security/TokenService.java
+++ src/main/java/com/lhama/lhamapersonalfinances/infra/security/TokenService.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.lhama.lhamapersonalfinances.model.entities.user.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -23,7 +24,7 @@ public class TokenService {
             String token = JWT.create()
                     .withIssuer("lhama-personal-finances")
                     .withSubject(user.getEmail())
-                    .withClaim("User id", user.getIdUser())
+                    .withClaim("idUser", user.getIdUser())
                     .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
             return token;
@@ -46,5 +47,22 @@ public class TokenService {
     }
     private Instant generateExpirationDate(){
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+    public Integer recoverIdFromToken(HttpHeaders header) {
+        String token = header.get("Authorization").get(0);
+        String jwt = token.replace("Bearer ", "");
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        try {
+            Integer idUser = JWT.require(algorithm)
+                    .withIssuer("lhama-personal-finances")
+                    .build()
+                    .verify(jwt)
+                    .getClaim("idUser")
+                    .asInt();
+            return idUser;
+        } catch (JWTVerificationException exception){
+            return null;
+        }
     }
 }
