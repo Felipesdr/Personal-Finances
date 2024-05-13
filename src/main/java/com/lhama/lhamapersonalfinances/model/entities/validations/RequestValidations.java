094587b88cc09597ab543a84b672ff6493deaebd
Felipesdr <felipederose@gmail.com>
2024-05-13 10:01:55 -0300

    Add methodo idUserValidation to RequestValidations

--- src/main/java/com/lhama/lhamapersonalfinances/model/entities/validations/RequestValidations.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/entities/validations/RequestValidations.java
@@ -1,10 +1,21 @@
 package com.lhama.lhamapersonalfinances.model.entities.validations;
 
 import com.lhama.lhamapersonalfinances.infra.exception.ValidationException;
+import com.lhama.lhamapersonalfinances.infra.security.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+
+import java.util.Objects;
 
 public class RequestValidations {
+    @Autowired
+    static TokenService tokenService;
 
     public static <T> void validateNullDTO(T dto) {
         if(dto == null) throw new ValidationException("The request body data cannot be null");
+    }
+
+    public static void idUserValidation(Integer idRequestingUser, Integer idUser){
+        if(!Objects.equals(idUser, idRequestingUser)) throw new ValidationException("Can´t do this for another user");
     }
 }
