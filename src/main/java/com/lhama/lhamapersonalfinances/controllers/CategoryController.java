45dc9cc1d3f803cbb4e25a2c9c15ef36dfd42d99
Felipesdr <felipederose@gmail.com>
2024-05-13 10:04:41 -0300

    Refactor methods from CategoryController

--- src/main/java/com/lhama/lhamapersonalfinances/controllers/CategoryController.java
+++ src/main/java/com/lhama/lhamapersonalfinances/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import com.lhama.lhamapersonalfinances.infra.exception.ValidationException;
 import com.lhama.lhamapersonalfinances.infra.exception.ValidationExceptionDTO;
 import com.lhama.lhamapersonalfinances.infra.security.TokenService;
 import com.lhama.lhamapersonalfinances.model.entities.category.*;
+import com.lhama.lhamapersonalfinances.model.entities.validations.RequestValidations;
 import com.lhama.lhamapersonalfinances.model.services.CategoryService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +26,11 @@ public class CategoryController {
 
     @PostMapping("register")
     @Transactional
-    public ResponseEntity registerGlobalCategory(@RequestBody CategoryRegisterDTO categoryRegisterData, UriComponentsBuilder uriBuilder){
-        try{
-            Category newCategory = categoryService.registerGlobalCategory(categoryRegisterData);
-            Integer newCategoryId = newCategory.getIdCategory();
-            URI uri = uriBuilder.path("register/category/{id}").buildAndExpand(newCategoryId).toUri();
-            return ResponseEntity.created(uri).body(new CategoryDTO(newCategory));
-        } catch (ValidationException e){
-            return ResponseEntity.badRequest().body(new ValidationExceptionDTO(e.getMessage()));
-        }
-
-    }
-
-    @PostMapping("register-user-created")
-    @Transactional
-    public ResponseEntity registerCategoryCreatedByUser(@RequestBody CategoryRegisterDTO categoryRegisterCreatedByUserData, UriComponentsBuilder uriBuilder, @RequestHeader HttpHeaders headers){
+    public ResponseEntity registerCategory(@RequestBody CategoryRegisterDTO categoryRegisterData, UriComponentsBuilder uriBuilder, @RequestHeader HttpHeaders headers){
         try {
+            RequestValidations.validateNullDTO(categoryRegisterData);
             Integer idUser = tokenService.recoverIdFromToken(headers);
-            Category newCategory = categoryService.registerCategoryCreatedByUser(categoryRegisterCreatedByUserData, idUser);
+            Category newCategory = categoryService.registerCategory(categoryRegisterData, idUser);
 
             Integer newCategoryId = newCategory.getIdCategory();
 
@@ -52,52 +40,40 @@ public class CategoryController {
         }catch (ValidationException e){
             return ResponseEntity.badRequest().body(new ValidationExceptionDTO(e.getMessage()));
         }
-
     }
 
-    @GetMapping("/{idUser}")
-    public ResponseEntity<List<CategoryDTO>> getAllCategoryByIdUserAndGlobal(@PathVariable Integer idUser){
-        if (idUser == null){
-            return ResponseEntity.badRequest().build();
-        }
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getAllCategoryByIdUserAndGlobal(@RequestHeader HttpHeaders headers){
+        Integer idUser = tokenService.recoverIdFromToken(headers);
 
         List<Category> temp = categoryService.getAllCategoryByIdUserAndGlobal(idUser);
-        List<CategoryDTO> categorys = temp.stream().map(c -> new CategoryDTO((Category) c)).toList();
+        List<CategoryDTO> categorys = temp.stream().map(CategoryDTO::new).toList();
 
         return ResponseEntity.ok().body(categorys);
     }
 
-
     @PutMapping("update")
     @Transactional
-    public ResponseEntity<CategoryDTO> updateCategoryById(@RequestBody CategoryUpdateDTO categoryUpdateData){
-        if (categoryUpdateData == null){
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity updateCategoryById(@RequestBody CategoryUpdateDTO categoryUpdateData, @RequestHeader HttpHeaders headers){
+        try{
+            RequestValidations.validateNullDTO(categoryUpdateData);
+            Integer idRequestingUser = tokenService.recoverIdFromToken(headers);
+            Category updatedCategory = categoryService.updateCategoryById(categoryUpdateData, idRequestingUser);
+            return ResponseEntity.ok(new CategoryDTO(updatedCategory));
+        } catch (ValidationException e){
+            return ResponseEntity.badRequest().body(new ValidationExceptionDTO(e.getMessage()));
         }
-
-        Category updatedCategory = categoryService.updateCategoryById(categoryUpdateData);
-        return ResponseEntity.ok(new CategoryDTO(updatedCategory));
-    }
-
-    @DeleteMapping("deactivate")
-    @Transactional
-    public ResponseEntity deactivateCategoryCreatedByUserById(@RequestBody CategoryDeactivateDTO categoryDeactivateData){
-        if(categoryDeactivateData == null){
-            return ResponseEntity.badRequest().build();
-        }
-
-        categoryService.deactivateCategoryCreatedByUserById(categoryDeactivateData);
-        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("deactivate/{idCategory}")
-    @Transactional
-    public ResponseEntity deactivateGlobalCategory(@PathVariable Integer idCategory){
-        if(idCategory == null){
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity deactivateCategoryById(@PathVariable Integer idCategory, @RequestHeader HttpHeaders headers){
+        try{
+            RequestValidations.validateNullDTO(idCategory);
+            Integer idRequestingUSer = tokenService.recoverIdFromToken(headers);
+            categoryService.deactivateCategoryCreatedByUserById(idCategory, idRequestingUSer);
+            return ResponseEntity.noContent().build();
+        } catch (ValidationException e){
+            return ResponseEntity.badRequest().body(new ValidationExceptionDTO(e.getMessage()));
         }
-
-        categoryService.deactivateGlobalCategory(idCategory);
-        return ResponseEntity.noContent().build();
     }
 }
