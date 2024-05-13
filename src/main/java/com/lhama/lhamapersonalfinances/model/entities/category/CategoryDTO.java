131d060fef36170bafac9325f067c873c770934d
Felipesdr <felipederose@gmail.com>
2024-05-13 10:03:54 -0300

    Refactor CategoryDTO.

--- src/main/java/com/lhama/lhamapersonalfinances/model/entities/category/CategoryDTO.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/entities/category/CategoryDTO.java
@@ -1,11 +1,18 @@
 package com.lhama.lhamapersonalfinances.model.entities.category;
 
 import com.lhama.lhamapersonalfinances.model.entities.user.User;
+import jakarta.annotation.Nullable;
 
-public record CategoryDTO(Integer idCategory, String name, boolean active, Integer idUser){
+public record CategoryDTO(Integer idCategory, String name, boolean active,
+                          @Nullable
+                          Integer idUser){
 
     public CategoryDTO(Category category){
-        this(category.getIdCategory(), category.getName(), category.isActive(), category.getUser().getIdUser());
+        this(
+                category.getIdCategory(),
+                category.getName(), category.isActive(),
+                category.getUser().getIdUser()
+        );
     }
 
 
