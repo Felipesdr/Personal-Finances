daa2e3b5e68459618d0fa789af558da35bb3eeec
Felipesdr <felipederose@gmail.com>
2024-05-14 12:01:32 -0300

    Delete ExpenseRepository.java

--- src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/ExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lhama.lhamapersonalfinances.model.repositorys;
-
-import com.lhama.lhamapersonalfinances.model.entities.expense.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseRepository extends JpaRepository<Expense, Integer> {
-}
