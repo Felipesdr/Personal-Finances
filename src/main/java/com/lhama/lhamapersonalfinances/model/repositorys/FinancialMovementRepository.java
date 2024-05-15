611b8cdeef2b86eba4122592fe1c1c25e8068270
Felipesdr <felipederose@gmail.com>
2024-05-15 08:34:23 -0300

    Refactor FinancialMovementRepository.

--- src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/FinancialMovementRepository.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/FinancialMovementRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FinancialMovementRepository extends JpaRepository<FinancialMovement, Integer> {
+public interface FinancialMovementRepository extends JpaRepository<FinancialMovement, Long> {
 }
