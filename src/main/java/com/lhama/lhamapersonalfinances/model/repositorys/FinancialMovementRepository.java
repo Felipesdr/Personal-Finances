c621f7f2b090b5992b7d21de0b70d2a68c1af94a
Felipesdr <felipederose@gmail.com>
2024-05-16 09:13:50 -0300

    Add methods: getUserTotalMonthlyExpenses, getUserTotalMonthlyIncomes, getUserMonthlyBalance to FinancialMovementRepository

--- src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/FinancialMovementRepository.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/FinancialMovementRepository.java
@@ -1,9 +1,50 @@
 package com.lhama.lhamapersonalfinances.model.repositorys;
 
 import com.lhama.lhamapersonalfinances.model.entities.financial_movements.FinancialMovement;
+import com.lhama.lhamapersonalfinances.model.entities.financial_movements.FinancialMovementType;
+import com.lhama.lhamapersonalfinances.model.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.YearMonth;
+import java.util.List;
 
 @Repository
 public interface FinancialMovementRepository extends JpaRepository<FinancialMovement, Long> {
+
+    @Query(value = """
+            select SUM(fm.value) from financial_movement fm
+            where fm.id_user = :idUser
+            and fm.type = "EXPENSE"
+            and YEAR(fm.date) = :year
+            and MONTH(fm.date) = :month
+            """, nativeQuery = true)
+    Double getUserTotalMonthlyExpenses(Long idUser, Integer year, Integer month);
+
+    @Query(value = """
+            select SUM(fm.value) from financial_movement fm
+            where fm.id_user = :idUser
+            and fm.type = "INCOME"
+            and YEAR(fm.date) = :year
+            and MONTH(fm.date) = :month
+            """, nativeQuery = true)
+    Double getUserTotalMonthlyIncomes(Long idUser, Integer year, Integer month);
+
+    @Query(value = """
+            select(
+                (select COALESCE(SUM(fm.value),0) from financial_movement fm
+                where fm.id_user = :idUser
+                and fm.type = "INCOME"
+                and YEAR(fm.date) = :year
+                and MONTH(fm.date) = :month)
+                -
+                (select COALESCE(SUM(fm.value),0) from financial_movement fm
+                where fm.id_user = :idUser
+                and fm.type = "EXPENSE"
+                and YEAR(fm.date) = :year
+                and MONTH(fm.date) = :month)
+            )
+            """, nativeQuery = true)
+    Double getUserMonthlyBalance(Long idUser, Integer year, Integer month);
 }
