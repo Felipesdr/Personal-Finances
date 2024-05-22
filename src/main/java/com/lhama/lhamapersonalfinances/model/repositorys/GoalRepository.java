ffcab949e1d23742f2350a0eccf776b6a48902b8
Felipesdr <felipederose@gmail.com>
2024-05-22 17:00:42 -0300

    Add findAllByUserAndActiveTrue method to GoalRepository

--- src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/GoalRepository.java
+++ src/main/java/com/lhama/lhamapersonalfinances/model/repositorys/GoalRepository.java
@@ -1,8 +1,11 @@
 package com.lhama.lhamapersonalfinances.model.repositorys;
 
 import com.lhama.lhamapersonalfinances.model.entities.goal.Goal;
+import com.lhama.lhamapersonalfinances.model.entities.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GoalRepository extends JpaRepository<Goal, Long> {
 
@@ -13,4 +16,6 @@ public interface GoalRepository extends JpaRepository<Goal, Long> {
             and g.active = true
             """, nativeQuery = true)
     public Goal getGoalByCategoryAndIdUser(Long idCategory, Long idUser);
+
+    public List<Goal> findAllByUserAndActiveTrue(User user);
 }
